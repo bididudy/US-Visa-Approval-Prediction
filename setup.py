727cc601f7d2d4794496a3eeebb1aa6c1b7d7ba3
Balaji Nalawade <balaji.nalawade@outlook.com>
2024-04-09 22:22:02 +0200

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Bididudy",
+    author_email="balaji.nalawade@outlook.com",
+    packages=find_packages()
+)
